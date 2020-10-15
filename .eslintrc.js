4dc8e7797dcceb3f114e8ca5151118849300df08
Antonio <antonimmo@users.noreply.github.com>
2020-10-14 23:07:59 -0700

    Chore: add linter conf

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-empty-function': ['error', { allow: ['constructors'] }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.json'],
+      rules: {
+        'comma-dangle': ['error', 'never']
+      }
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
+};
